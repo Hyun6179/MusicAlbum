96a0dc9ec2f79c2c3fb63af6bea34b92e2ec2ec6
Hyun6179 <lhm6179@naver.com>
2023-05-03 11:07:12 +0900

    commit -- 미완

--- app/src/main/java/com/example/musicalbum/AudioImage.java
+++ app/src/main/java/com/example/musicalbum/AudioImage.java
@@ -1,14 +1,62 @@
 package com.example.musicalbum;
 
+import android.content.Intent;
+import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AudioImage extends AppCompatActivity {
+
+    MediaPlayer mp = new MediaPlayer();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.audio_image);
+        Intent it = getIntent();
+        String tag = it.getStringExtra("it_tag");
+
+        TextView title = findViewById(R.id.title);
+        TextView lyrics = findViewById(R.id.lyrics);
+        ImageView song_image = findViewById(R.id.song_image);
+
+        Resources res = getResources();
+
+        int stringId;
+        String mkKey;
+
+        stringId = res.getIdentifier("title"+tag, "string",getPackageName());
+        //mkKey = res.getString(stringId);
+        title.setText(stringId);
+
+        stringId = res.getIdentifier("lyrics"+tag, "string",getPackageName());
+        //mkKey = res.getString(stringId);
+        lyrics.setText(stringId);
+
+
+        stringId = res.getIdentifier("song_image"+tag, "string",getPackageName());
+        mkKey = res.getString(stringId);
+        int imageId = res.getIdentifier(mkKey,"drawable",getPackageName());
+        song_image.setImageResource(imageId);
+
+        stringId = res.getIdentifier("audio"+tag,"string",getPackageName());
+        mkKey = res.getString(stringId);
+        int audioId = res.getIdentifier(mkKey,"raw",getPackageName());
+        mp = MediaPlayer.create(this,audioId);
+        mp.setLooping(false);
+        mp.start();
+    }
+
+    public void goBack(View view) {
+        if(mp.isPlaying()){
+            mp.start();
+            mp.release();
+        }
+        super.onDestroy();
     }
 }
--- app/src/main/java/com/example/musicalbum/MainActivity.java
+++ app/src/main/java/com/example/musicalbum/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.musicalbum;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("노래 제목");
+    }
+
+    public void play(View view) {
+        LinearLayout layout = findViewById(view.getId());
+        String tag = layout.getTag().toString();
+
+        Intent it = new Intent(this, AudioImage.class);
+        it.putExtra("it_tag", tag);
+        startActivity(it);
     }
 }
